636fee63a9e2e1dfac237687af5b4634133f2159
Ricard Sanchez <ricardsanchez50@gmail.com>
2019-10-14 14:17:09 +0200

    Spa: Añadido cambios en scripts

--- scripts/make-delivery.sh
+++ scripts/make-delivery.sh
@@ -3,7 +3,6 @@ project_dir='/mnt/c/Users/ricar_xuzvwqa/Codigo/regression_classifier'
 docs='/mnt/c/Users/ricar_xuzvwqa/OneDrive/Documentos/Asignaturas/Primer Semestre - 2019-2020/Aprendizaje Computacional/Prácticas/Documentos'
 project_name="${project_dir##*/}"
 project_dir2="${project_dir%/*}"
-#### Copy project
 pushd . > /dev/null
 cd "${project_dir}"
 cd ..
@@ -13,7 +12,6 @@ cp -r "${project_dir}" .
 popd > /dev/null 
 mv "${project_dir2}/tmp/${project_name}" .
 rm -d "${project_dir2}/tmp/"
-#### Filter and prepare project
 cd "${project_name}"
 rm -r .git/
 if (("$project_name" == 'regression_classifier'))
@@ -22,7 +20,6 @@ then
 	rm docs/dataset_web.html
 fi
 cd ..
-#### Zip
 zip -r "${project_name}.zip" "${project_name}" 
 rm -r "${project_name}" 
 if (("$project_name" == 'regression_classifier'))
